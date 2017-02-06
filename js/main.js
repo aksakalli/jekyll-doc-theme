99a17c59181178f3a0da3b85afa0384086426cd1
can Guney Aksakalli <cgaksakalli@gmail.com>
2017-02-06 20:13:05 +0100

    accordion doc navigation & search

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function() {
+
+    // $('.collapse').collapse('hide');
+    $('.list-group-item.active').parent().parent('.collapse').collapse('show');
+
+
+    var pages = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+        // datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+
+        // local: ['dog', 'pig', 'moose'],
+
+        prefetch: '/search.json'
+    });
+
+    $('#search-box').typeahead({
+        minLength: 0,
+        highlight: true
+    }, {
+        name: 'pages',
+        display: 'title',
+        source: pages
+    });
+
+    $('#search-box').bind('typeahead:select', function(ev, suggestion) {
+        window.location.href = suggestion.url;
+    });
+
+});
